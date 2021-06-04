5a596a9cacf31631eb77ccc0cd921a734972e3e4
Fatima Jawed <55167453+fzjawed@users.noreply.github.com>
2021-06-04 14:29:53 +0300

    Add files via upload

---
+++ Index-with-Left-and-Right-Sum.py
@@ -0,0 +1,10 @@
+class Solution:
+    def solve(self, nums):
+        sum_left = 0
+        sum_right = sum(nums)
+        for i in range(len(nums)):
+            sum_right -= nums[i]
+            if sum_left == sum_right:
+                return i
+            sum_left += nums[i]
+        return -1
